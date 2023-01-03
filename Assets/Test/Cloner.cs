ec21b15e17774a9cdfca566425a514ab4c456e63
Keijiro Takahashi <keijiro@gmail.com>
2023-01-03 18:43:01 +0900

    Benchmark

---
+++ Assets/Test/Cloner.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Cloner : MonoBehaviour
+{
+    [SerializeField] GameObject _source = null;
+    [SerializeField] int _count = 10;
+    [SerializeField] float _range = 1;
+
+    void Start()
+    {
+        for (var i = 0; i < _count; i++)
+        {
+            var p = Random.insideUnitCircle * _range;
+            var r = Random.rotation;
+            Instantiate(_source, new Vector3(p.x, 0, p.y), r);
+        }
+    }
+}
