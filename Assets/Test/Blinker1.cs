803a5e258814ca3194e970142d7c766243f0d489
Keijiro Takahashi <keijiro@gmail.com>
2023-01-04 14:27:37 +0900

    Color animation

--- Assets/Test/Blinker1.cs
+++ Assets/Test/Blinker1.cs
@@ -2,8 +2,11 @@ using UnityEngine;
 
 public sealed class Blinker1 : MonoBehaviour
 {
-    static Color AnimatedColor
-      => Color.Lerp(Color.red, Color.blue, (Mathf.Sin(Time.time) + 1) / 2);
+    float AnimationTime
+      => (GetInstanceID() & 0xff) / 256.0f + Time.time / 5;
+
+    Color AnimatedColor
+      => Color.HSVToRGB(AnimationTime % 1, 0.9f, 1);
 
     void Update()
       => GetComponent<Renderer>().material.color = AnimatedColor;
