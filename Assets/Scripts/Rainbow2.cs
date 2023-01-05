815e3f3cf42741c7e9cc78fae61736b18200e0c0
Keijiro Takahashi <keijiro@gmail.com>
2023-01-05 15:44:00 +0900

    Rename

--- Assets/Scripts/Rainbow2.cs
+++ Assets/Scripts/Rainbow2.cs
@@ -1,9 +1,9 @@
 using UnityEngine;
 
-public sealed class Blinker2 : MonoBehaviour
+public sealed class Rainbow2 : MonoBehaviour
 {
     float AnimationTime
-      => (GetInstanceID() & 0xff) / 256.0f + Time.time / 5;
+      => (GetInstanceID() & 0xff) / 256.0f + Time.time / 3;
 
     Color AnimatedColor
       => Color.HSVToRGB(AnimationTime % 1, 0.9f, 1);
